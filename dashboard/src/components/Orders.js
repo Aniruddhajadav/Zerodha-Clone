5003d3dabad5eca53b18795dedcd9f4347902df6
Aniruddha Jadav <mpparmar1972@gmail.com>
2026-06-07 18:59:38 +0530

    Fix embedded repo and add dashboard files

---
+++ dashboard/src/components/Orders.js
@@ -0,0 +1,58 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+
+const Orders = () => {
+  const [allOrders, setallOrders] = useState([]);
+
+  useState(() => {
+    axios.get("http://localhost:3001/allOrders").then((res) => {
+      console.log(res.data);
+      setallOrders(res.data);
+    });
+  }, []);
+
+  if (allOrders.length === 0) {
+    return (
+      <div className="orders">
+        <div className="no-orders">
+          <p>You haven't placed any orders today</p>
+
+          <Link to={"/"} className="btn">
+            Get started
+          </Link>
+        </div>
+      </div>
+    );
+  }
+
+  return (
+    <>
+      <h3 className="title">Orders ({allOrders.length})</h3>
+
+      <div className="order-table">
+        <table>
+          <tr>
+            <th>Instrument</th>
+            <th>qty</th>
+            <th>Price</th>
+            <th>Mode</th>
+          </tr>
+
+          {allOrders.map((stock, index) => {
+            return (
+              <tr key={index}>
+                <td>{stock.name}</td>
+                <td>{stock.qty}</td>
+                <td>{stock.price.toFixed(2)}</td>
+                <td>{stock.mode}</td>
+              </tr>
+            );
+          })}
+        </table>
+      </div>
+    </>
+  );
+};
+
+export default Orders;

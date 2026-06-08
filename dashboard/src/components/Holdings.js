5fa699853a61bb31b4eb7426dfc0542cb39026fc
Aniruddha Jadav <mpparmar1972@gmail.com>
2026-06-08 22:47:33 +0530

    PORT Add fix

--- dashboard/src/components/Holdings.js
+++ dashboard/src/components/Holdings.js
@@ -8,10 +8,12 @@ const Holdings = () => {
   const [allHoldings, setAllHoldings] = useState([]);
 
   useState(() => {
-    axios.get("http://localhost:3001/allHoldings").then((res) => {
-      console.log(res.data);
-      setAllHoldings(res.data);
-    });
+    axios
+      .get("https://zerodha-clone-s76j.onrender.com/allHoldings")
+      .then((res) => {
+        console.log(res.data);
+        setAllHoldings(res.data);
+      });
   }, []);
 
   const labels = allHoldings.map((subArray) => subArray["name"]);

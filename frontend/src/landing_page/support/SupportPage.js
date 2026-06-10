12abf4f5bec1adefa1cf704e03e867f03e19f877
Aniruddha Jadav <mpparmar1972@gmail.com>
2026-06-10 19:52:18 +0530

    NEW A FIX

--- frontend/src/landing_page/support/SupportPage.js
+++ frontend/src/landing_page/support/SupportPage.js
@@ -3,6 +3,29 @@ import React from "react";
 import Hero from "./Hero";
 import CreateTicket from "./CreateTicket";
 
+const API_URL = process.env.REACT_APP_API_URL || "http://localhost:3001";
+
+useEffect(() => {
+  const verifyCookie = async () => {
+    if (!cookies.token) {
+      navigate("/login");
+    }
+    const { data } = await axios.post(
+      `${API_URL}/support`,
+      {},
+      { withCredentials: true },
+    );
+    const { status, user } = data;
+    setUsername(user);
+    return status
+      ? toast(`Hello ${user}`, {
+          position: "top-right",
+        })
+      : (removeCookie("token"), navigate("/login"));
+  };
+  verifyCookie();
+}, [cookies, navigate, removeCookie]);
+
 function SupportPage() {
   return (
     <>

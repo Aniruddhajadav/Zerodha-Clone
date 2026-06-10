134c57c8aebbf90dd3bfad3f94e5f9cc6885bb3f
Aniruddha Jadav <mpparmar1972@gmail.com>
2026-06-10 20:03:24 +0530

    SupoortPage New Fix

--- frontend/src/landing_page/support/SupportPage.js
+++ frontend/src/landing_page/support/SupportPage.js
@@ -11,31 +11,31 @@ import { ToastContainer, toast } from "react-toastify";
 
 const API_URL = process.env.REACT_APP_API_URL || "http://localhost:3001";
 
-useEffect(() => {
-  const verifyCookie = async () => {
-    if (!cookies.token) {
-      navigate("/login");
-    }
-    const { data } = await axios.post(
-      `${API_URL}/support`,
-      {},
-      { withCredentials: true },
-    );
-    const { status, user } = data;
-    setUsername(user);
-    return status
-      ? toast(`Hello ${user}`, {
-          position: "top-right",
-        })
-      : (removeCookie("token"), navigate("/login"));
-  };
-  verifyCookie();
-}, [cookies, navigate, removeCookie]);
-
 function SupportPage() {
   const navigate = useNavigate();
   const [cookies, removeCookie] = useCookies([]);
   const [username, setUsername] = useState("");
+
+  useEffect(() => {
+    const verifyCookie = async () => {
+      if (!cookies.token) {
+        navigate("/login");
+      }
+      const { data } = await axios.post(
+        `${API_URL}/support`,
+        {},
+        { withCredentials: true },
+      );
+      const { status, user } = data;
+      setUsername(user);
+      return status
+        ? toast(`Hello ${user}`, {
+            position: "top-right",
+          })
+        : (removeCookie("token"), navigate("/login"));
+    };
+    verifyCookie();
+  }, [cookies, navigate, removeCookie]);
 
   return (
     <>

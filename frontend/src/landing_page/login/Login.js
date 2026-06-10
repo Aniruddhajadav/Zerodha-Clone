51443bc1ea1e06bc1ace8fa3acc771ea985dbf49
Aniruddha Jadav <mpparmar1972@gmail.com>
2026-06-10 22:21:40 +0530

    New Fixed

--- frontend/src/landing_page/login/Login.js
+++ frontend/src/landing_page/login/Login.js
@@ -44,7 +44,7 @@ const Login = () => {
       if (success) {
         handleSuccess(message);
         setTimeout(() => {
-          navigate("");
+          navigate("/");
         }, 3000);
       } else {
         handleError(message);

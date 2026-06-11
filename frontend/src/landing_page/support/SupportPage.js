cbe0ff1150a6289a3311d4a5694a395e81b7cfaa
Aniruddha Jadav <mpparmar1972@gmail.com>
2026-06-11 16:19:05 +0530

    NEW Fixed

--- frontend/src/landing_page/support/SupportPage.js
+++ frontend/src/landing_page/support/SupportPage.js
@@ -3,33 +3,33 @@ import React from "react";
 import Hero from "./Hero";
 import CreateTicket from "./CreateTicket";
 
-import { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
-import { useCookies } from "react-cookie";
-import axios from "axios";
-import { ToastContainer, toast } from "react-toastify";
+// import { useEffect, useState } from "react";
+// import { useNavigate } from "react-router-dom";
+// import { useCookies } from "react-cookie";
+// import axios from "axios";
+// import { ToastContainer, toast } from "react-toastify";
 
 function SupportPage() {
-  const navigate = useNavigate();
-  const [cookies, removeCookie] = useCookies([]);
-  const [username, setUsername] = useState("");
+  // const navigate = useNavigate();
+  // const [cookies, removeCookie] = useCookies([]);
+  // const [username, setUsername] = useState("");
 
-  useEffect(() => {
-    const verifyCookie = async () => {
-      if (!cookies.token) {
-        navigate("/login");
-      }
-      const { data } = await axios.post("/", {}, { withCredentials: true });
-      const { status, user } = data;
-      setUsername(user);
-      return status
-        ? toast(`Hello ${user}`, {
-            position: "top-right",
-          })
-        : (removeCookie("token"), navigate("/login"));
-    };
-    verifyCookie();
-  }, [cookies, navigate, removeCookie]);
+  // useEffect(() => {
+  //   const verifyCookie = async () => {
+  //     if (!cookies.token) {
+  //       navigate("/login");
+  //     }
+  //     const { data } = await axios.post("/", {}, { withCredentials: true });
+  //     const { status, user } = data;
+  //     setUsername(user);
+  //     return status
+  //       ? toast(`Hello ${user}`, {
+  //           position: "top-right",
+  //         })
+  //       : (removeCookie("token"), navigate("/login"));
+  //   };
+  //   verifyCookie();
+  // }, [cookies, navigate, removeCookie]);
 
   return (
     <>
